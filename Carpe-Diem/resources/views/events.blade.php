e2358311af3084dd1c811b3d2584d6056bfa9e62
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-03-31 15:12:55 +0200

    Listing out the events

--- Carpe-Diem/resources/views/events.blade.php
+++ Carpe-Diem/resources/views/events.blade.php
@@ -12,39 +12,17 @@
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
-            src="images/acme.png"
+            src="images/no_image.png"
             alt=""
         />
         <div>
             <h3 class="text-2xl">
-                <a href="show.html">Senior Laravel Developer</a>
+                <a href=""> {{-- {{ $event->title }} --}} Event title </a>
             </h3>
-            <div class="text-xl font-bold mb-4">Acme Corp</div>
-            <ul class="flex">
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Laravel</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">API</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Backend</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
+            <div class="text-xl font-bold mb-4">Something Corp</div>
             <div class="text-lg mt-4">
-                <i class="fa-solid fa-location-dot"></i> Boston,
-                MA
+                <i class="fa-solid fa-location-dot"></i> Eger
+                
             </div>
         </div>
     </div>
---
+++ Carpe-Diem/resources/views/events/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('content')
+   @include('partials.hero')
+   {{-- @include('partials._search') --}} 
+
+    {{--<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
+        @if (count($events) == 0)
+            <p>
+                No event found
+            </p>
+        @endif
+        @foreach ($events as $event)
+            <h1>alma</h1>
+        @endforeach
+    </div>  --}}
+
+@endsection
