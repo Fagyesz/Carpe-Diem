5714f820df5568771f00878a1950e9b0b31b2395
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-03-31 20:14:21 +0200

    Create a card for an event

--- Carpe-Diem/resources/views/events/create.blade.php
+++ Carpe-Diem/resources/views/events/create.blade.php
@@ -1,126 +1,76 @@
-<x-layout>
-<div
-class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24"
->
-<header class="text-center">
-    <h2 class="text-2xl font-bold uppercase mb-1">
-        Create a Gig
-    </h2>
-    <p class="mb-4">Post a gig to find a developer</p>
-</header>
+@extends('layout')
 
-<form action="">
-    <div class="mb-6">
-        <label
-            for="company"
-            class="inline-block text-lg mb-2"
-            >Company Name</label
-        >
-        <input
-            type="text"
-            class="border border-gray-200 rounded p-2 w-full"
-            name="company"
-        />
-    </div>
+@section('content')
+    <x-card class="p-10 max-w-lg mx-auto mt-24">
 
-    <div class="mb-6">
-        <label for="title" class="inline-block text-lg mb-2"
-            >Job Title</label
-        >
-        <input
-            type="text"
-            class="border border-gray-200 rounded p-2 w-full"
-            name="title"
-            placeholder="Example: Senior Laravel Developer"
-        />
-    </div>
+        <header class="text-center">
+            <h2 class="text-2xl font-bold uppercase mb-1">
+                Create
+            </h2>
+            <p class="mb-4">Post a new Event</p>
+        </header>
 
-    <div class="mb-6">
-        <label
-            for="location"
-            class="inline-block text-lg mb-2"
-            >Job Location</label
-        >
-        <input
-            type="text"
-            class="border border-gray-200 rounded p-2 w-full"
-            name="location"
-            placeholder="Example: Remote, Boston MA, etc"
-        />
-    </div>
+        <form method="POST" action="/events" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-6">
+                <label for="title" class="inline-block text-lg mb-2">Title</label>
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
+                    value="{{ old('title') }}" placeholder="Example: Super cool T-shirt" />
+                @error('title')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
 
-    <div class="mb-6">
-        <label for="email" class="inline-block text-lg mb-2"
-            >Contact Email</label
-        >
-        <input
-            type="text"
-            class="border border-gray-200 rounded p-2 w-full"
-            name="email"
-        />
-    </div>
+            <div class="mb-6">
+                <label for="title" class="inline-block text-lg mb-2">Price</label>
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="price"
+                    value="{{ old('price') }}" placeholder="Ft" />
+                @error('title')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
 
-    <div class="mb-6">
-        <label
-            for="website"
-            class="inline-block text-lg mb-2"
-        >
-            Website/Application URL
-        </label>
-        <input
-            type="text"
-            class="border border-gray-200 rounded p-2 w-full"
-            name="website"
-        />
-    </div>
+            <div class="mb-6">
+                <label for="tags" class="inline-block text-lg mb-2">
+                    Tags (Comma Separated)
+                </label>
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
+                    value="{{ old('tags') }}" placeholder="Example: Black, Latest model, etc" />
+                @error('tags')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
 
-    <div class="mb-6">
-        <label for="tags" class="inline-block text-lg mb-2">
-            Tags (Comma Separated)
-        </label>
-        <input
-            type="text"
-            class="border border-gray-200 rounded p-2 w-full"
-            name="tags"
-            placeholder="Example: Laravel, Backend, Postgres, etc"
-        />
-    </div>
+            <div class="mb-6">
+                <label for="logo" class="inline-block text-lg mb-2">
+                    Image
+                </label>
+                <input type="file" class="border border-gray-200 rounded p-2 w-full" name="logo" />
+                @error('logo')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
 
-    <div class="mb-6">
-        <label for="logo" class="inline-block text-lg mb-2">
-            Company Logo
-        </label>
-        <input
-            type="file"
-            class="border border-gray-200 rounded p-2 w-full"
-            name="logo"
-        />
-    </div>
 
-    <div class="mb-6">
-        <label
-            for="description"
-            class="inline-block text-lg mb-2"
-        >
-            Job Description
-        </label>
-        <textarea
-            class="border border-gray-200 rounded p-2 w-full"
-            name="description"
-            rows="10"
-            placeholder="Include tasks, requirements, salary, etc"
-        ></textarea>
-    </div>
+            <div class="mb-6">
+                <label for="description" class="inline-block text-lg mb-2">
+                    Description
+                </label>
+                <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10"
+                    placeholder="Made from 100% cotton, black, made in Italy, etc...">{{ old('description') }}</textarea>
+                @error('description')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
 
-    <div class="mb-6">
-        <button
-            class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
-        >
-            Create Gig
-        </button>
+            <div class="mb-6">
+                <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
+                    Create T-shirt
+                </button>
 
-        <a href="/" class="text-black ml-4"> Back </a>
-    </div>
-</form>
-</div>
-</x-layout>
+                <a href="/" class="text-black ml-4"> Back </a>
+            </div>
+        </form>
+        </div>
+    </x-card>
+@endsection
