725120c2bca315c8c751ca4b2cf37eaea49df549
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-03-31 15:18:24 +0200

    Adding search bar

--- Carpe-Diem/resources/views/events/index.blade.php
+++ Carpe-Diem/resources/views/events/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
    @include('partials.hero')
-   {{-- @include('partials._search') --}} 
+   @include('partials.search')
 
     {{--<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
