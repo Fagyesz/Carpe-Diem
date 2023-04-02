9ddc1af3109fbd231f2e3f378cd3dcbc8069f959
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-04-03 00:49:47 +0200

    Load  create form data to database

--- Carpe-Diem/database/migrations/2023_03_27_124914_create_Event_table.php
+++ Carpe-Diem/database/migrations/2023_03_27_124914_create_Event_table.php
@@ -7,16 +7,17 @@ class CreateEventTable extends Migration {
 
 	public function up()
 	{
-		Schema::create('Event', function(Blueprint $table) {
+		Schema::create('events', function(Blueprint $table) {
 			$table->increments('id');
 			$table->string('title', 255);
-			$table->text('description');
-			$table->datetime('start_time');
-			$table->datetime('end_time');
 			$table->string('location', 255);
 			$table->string('organizer', 255);
 			$table->decimal('ticket_price', 15);
 			$table->integer('tickets_available');
+			$table->text('description');
+			$table->datetime('start_time');
+			$table->datetime('end_time');
+		
 		});
 	}
 
