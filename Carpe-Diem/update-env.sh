2b85f980e45f8a29eaa35c7c85c82d858a81e564
Vincze Flórián <44016005+Fagyesz@users.noreply.github.com>
2023-04-06 09:08:38 +0000

    Add socials and update secreat script

--- Carpe-Diem/update-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get the values of the secrets from GitHub Secrets
-DB_HOST="${{ secrets.DB_HOST }}"
-DB_PASSWORD="${{ secrets.DB_PASSWORD }}"
-
-# Update the .env file with the secret values
-sed -i "s/DB_HOST=.*/DB_HOST=${DB_HOST}/" .env
-sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=${DB_PASSWORD}/" .env
