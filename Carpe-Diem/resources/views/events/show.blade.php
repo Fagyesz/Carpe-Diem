43b8c3934ee9be3f7ddd24b441686c442f477654
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-04-16 12:17:22 +0200

    Adjusting the single listing page

--- Carpe-Diem/resources/views/events/show.blade.php
+++ Carpe-Diem/resources/views/events/show.blade.php
@@ -4,33 +4,35 @@
 <body class="bg-scroll" style="background-image:url({{ url('images/single_listing_bg.jpg') }}); background-size: cover; background-position: center center">
     <div class="mx-6" style="margin-left: 30rem; margin-right: 30rem; margin-top: 5%; ">
         <x-card class="p-5">
-            <div class="flex flex-col items-center justify-center text-center">
+            <div class="mx-20 flex flex-col justify-items-start text-start">
                 <img class="hidden w-80 mr-6 md:block"
             src="{{ $event->event_image ? asset('storage/' . $event->event_image) : asset('/images/no_image.jpg') }}"
             alt="" />
 
-                <h3 class="text-2xl mb-2">{{ $event->title }}</h3>
+                <h3 class=" flex justify-center text-2xl font-bold mb-2">{{ $event->title }}</h3>
                 <div class="text-xl mb-4"> <i class='fa-solid fa-location-dot'></i> Location: {{ $event->location }} </div>
-                <div class="text-xl mb-4"><i class="fa-solid fa-person"></i> Organizer: {{ $organizer[0] }} </div>
+                <div class="text-xl mb-4"><h3><i class="fa-solid fa-person"></i>Organizer: {{ $organizer[0] }} </h3></div>
                 <div class="text-xl mb-4"><i class="fa-solid fa-hourglass-start"></i> Starting time: {{ Carbon\Carbon::parse($event->start_time)->format('Y.m.d h:m')}} </div>
                 <div class="text-xl mb-4"> <i class="fa-solid fa-flag-checkered"></i> Ending time: {{ Carbon\Carbon::parse($event->end_time)->format('Y.m.d h:m') }} </div>
                 <div class="text-xl mb-4"> <i class="fa-solid fa-clock"></i> Duration: {{ $totalDuration}} </div>
 
 
 
-                <div class="text-xl font-bold mb-4">Ticket price: {{ $event->ticket_price }} €</div>
+                <div class="flex justify-center text-xl font-bold mb-4">Ticket price: {{ $event->ticket_price }} €</div>
                 <div class="border border-gray-200 w-full mb-6"></div>
-                    <h3 class="text-3xl font-bold mb-4">
+                    <h3 class="flex justify-center text-3xl font-bold mb-4">
                         Description 
                     </h3>
-                    <div class="text-lg space-y-6">
-                        <p>{{ $event->description }}<p>
+                <div class="text-xl mb-4">
+                    <p>{{ $event->description }}<p>
+                </div>
+                    <div class="flex justify-center text-lg space-y-6">
                             @csrf
                             @method('POST')
                                 <input type="hidden" value="{{ $event }}" name="id">
                                 <button>
                                     <a target="_blank"
-                                       class="block bg-black text-white py-3 px-2 rounded-xl hover:opacity-80">
+                                       class=" block bg-black text-white py-3 px-2 rounded-xl hover:opacity-80">
                                         <i class="fa fa-cart-plus"
                                            aria-hidden="true">
                                         </i>
