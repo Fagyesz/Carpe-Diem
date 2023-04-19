3b692d1dd9c5055e6df316514967ac38a949935e
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-04-19 22:03:36 +0200

    Giving logged in user variable to the blade

--- Carpe-Diem/app/Http/Controllers/UserController.php
+++ Carpe-Diem/app/Http/Controllers/UserController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function show(User $user) 
     {
-        return view('auth.profile');
+
+        return view('auth.profile', ['user'=> Auth::user()]);
     }
 }
