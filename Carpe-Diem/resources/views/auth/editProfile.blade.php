4754968f5d7464cd5474d2df1731d8a6f3c85c0e
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-04-20 20:54:49 +0200

    Adding profile picture to the form

--- Carpe-Diem/resources/views/auth/editProfile.blade.php
+++ Carpe-Diem/resources/views/auth/editProfile.blade.php
@@ -3,18 +3,20 @@
 @section('content')
 <div class="flex bg-cover bg-center bg-no-repeat" style="background-image: url( '../images/single_listing_bg.jpg')">
     <div class="w-full lg:w-4/12 px-4 mx-auto pt-6">
-      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16 opacity-90">
+        <div
+            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16 opacity-90">
             <div class="flex px-16 py-8 ">
                 <form method="POST" action="/profile/edit" enctype="multipart/form-data">
                     @csrf
                     @method('PUT')
-                    <div class="flex justify-center text-center text-2xl font-bold mb-16 pl-4">Edit your personal datas</div>
+                    <div class="flex justify-center text-center text-2xl font-bold mb-16 pl-4">Edit your personal datas
+                    </div>
                     <div class="mb-6 pl-4">
                         <label for="username" class="inline-block text-lg mb-2">Username:</label>
                         <input type="text" class="border border-gray-200 rounded p-2 w-full" name="username"
                             value="{{ $user->username }}" />
                         @error('username')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="mb-6 pl-4">
@@ -22,7 +24,7 @@
                         <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name"
                             value="{{ $user->name }}" />
                         @error('name')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="mb-6 pl-4">
@@ -30,7 +32,7 @@
                         <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email"
                             value="{{ $user->email }}" />
                         @error('email')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="mb-6 pl-4">
@@ -38,15 +40,16 @@
                         <input type="date" class="border border-gray-200 rounded p-2 w-full" name="birthdate"
                             value="{{ $user->birthdate }}" />
                         @error('birthdate')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="mb-6 pl-4">
                         <label for="phone" class="inline-block text-lg mb-2">Phone:</label>
-                        <input type="tel" pattern="[0-9]{2}-[0-9]{2}-[0-9]{3}-[0-9]{4}" class="border border-gray-200 rounded p-2 w-full" name="phone"
+                        <input type="tel" pattern="[0-9]{2}-[0-9]{2}-[0-9]{3}-[0-9]{4}"
+                            class="border border-gray-200 rounded p-2 w-full" name="phone"
                             placeholder="Format: 00-11-222-3333" value="{{ $user->phone }}" />
                         @error('phone')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="mb-6 pl-4">
@@ -54,7 +57,7 @@
                         <input type="text" class="border border-gray-200 rounded p-2 w-full" name="country"
                             value="{{ $user->country }}" />
                         @error('country')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="mb-6 pl-4">
@@ -62,35 +65,63 @@
                         <input type="text" class="border border-gray-200 rounded p-2 w-full" name="address"
                             value="{{ $user->address }}" />
                         @error('address')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="py-4 pl-4">
                         <div class="flex items-center mb-4">
-                            <input id="male" type="radio" value="Male" name="gender" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" @checked($user->gender == "Male")>
+                            <input id="male" type="radio" value="Male" name="gender"
+                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                @checked($user->gender == "Male")>
                             <label for="male" class="ml-2 text-sm font-medium ">Male</label>
                         </div>
                         <div class="flex items-center">
-                            <input id="female" type="radio" value="Female" name="gender" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" @checked($user->gender == "Female")>
-                            <label for="female" class="ml-2 text-sm font-medium  " @checked($user->gender == "Female")>Female</label>
+                            <input id="female" type="radio" value="Female" name="gender"
+                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                @checked($user->gender == "Female")>
+                            <label for="female" class="ml-2 text-sm font-medium  " @checked($user->gender ==
+                                "Female")>Female</label>
                         </div>
                     </div>
                     <div class="mb-6 pl-4">
                         <label for="bio" class="inline-block text-lg mb-2">Bio:</label>
-                        <textarea type="text" class="border border-gray-200 rounded p-2 w-full resize-none" name="bio" rows="10" >{{ $user->bio }}</textarea>
+                        <textarea type="text" class="border border-gray-200 rounded p-2 w-full resize-none" name="bio"
+                            rows="10">{{ $user->bio }}</textarea>
                         @error('bio')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                         @enderror
                     </div>
-                    <div class="mb-6 pt-6 pl-4 flex justify-center">
-                        <div class="flex pr-4"><a href="/profile"> <i class="fa-solid fa-backward text-3xl max-w-xs transition duration-300 ease-in-out hover:scale-125"></i></a></div>
-                        <button class="bg-laravel text-black rounded py-2 px-4 hover:bg-black text-white ">
-                            Edit profile 
-                        </button>
+                    <div class="mb-6 pl-4">
+                        <label for="avatar" class="inline-block text-lg mb-2">
+                            Avatar
+                        </label>
+                        <input type="file" class="border border-gray-200 rounded p-2 w-full" name="avatar" />
+    
+                        <img class="hidden w-80 mr-6 md:block"
+                            src="{{ $user->avatar ? asset('storage/' . $user->avatar) : asset('/images/no_avatar.jpg') }}"
+                            alt="" />
+    
+                        @error('event_image')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div class="grid justify-items-stretch">
+                        <div class="flex flex-row">
+                            <div class="justify-self-start pr-4"><a href="/profile"> <i
+                                        class="flex justify-start fa-solid fa-backward text-3xl max-w-xs transition duration-300 ease-in-out hover:scale-125"></i></a>
+                            </div>
+                            <div class="justify-self-stretch ml-16">
+                                <button class="bg-laravel text-black rounded py-2 px-4 hover:bg-black text-white ">
+                                    Edit profile
+                                </button>
+                            </div>
+                        </div>
+
+
                     </div>
                 </form>
             </div>
-      </div>
+        </div>
     </div>
 </div>
 @endsection
--- Carpe-Diem/resources/views/events/edit.blade.php
+++ Carpe-Diem/resources/views/events/edit.blade.php
@@ -1,7 +1,9 @@
 @extends('layout')
 
- @section('content')
- <body class="bg-scroll" style="background-image:url({{ url('images/single_listing_bg.jpg') }}); background-size: cover; background-position: center center">
+@section('content')
+
+<body class="bg-scroll"
+    style="background-image:url({{ url('images/single_listing_bg.jpg') }}); background-size: cover; background-position: center center">
 
     <x-card class="p-10 max-w-lg mx-auto mt-24">
 
@@ -12,110 +14,111 @@
             <p class="mb-4">Edit the current event</p>
         </header>
         <div class="p-4">
-        <form method="POST" action="/events/{{ $event->id }}" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-            <div class="mb-6">
-                <label for="title" class="inline-block text-lg mb-2">Title</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
-                    value="{{ $event->title }}" placeholder="Example: XY Concert" />
-                @error('title')
+            <form method="POST" action="/events/{{ $event->id }}" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="mb-6">
+                    <label for="title" class="inline-block text-lg mb-2">Title</label>
+                    <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
+                        value="{{ $event->title }}" placeholder="Example: XY Concert" />
+                    @error('title')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            <div class="mb-6">
-                <label for="location" class="inline-block text-lg mb-2">Location</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
-                    value="{{ $event->location }}" placeholder="Example: Eger, XY Sörkert" />
-                @error('location')
+                <div class="mb-6">
+                    <label for="location" class="inline-block text-lg mb-2">Location</label>
+                    <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
+                        value="{{ $event->location }}" placeholder="Example: Eger, XY Sörkert" />
+                    @error('location')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            {{-- <div class="mb-6">
-                <label for="organizer" class="inline-block text-lg mb-2">Organizer</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="organizer"
-                    value="{{ $event->organizer }}" placeholder="" />
-                @error('organizer')
+                {{-- <div class="mb-6">
+                    <label for="organizer" class="inline-block text-lg mb-2">Organizer</label>
+                    <input type="text" class="border border-gray-200 rounded p-2 w-full" name="organizer"
+                        value="{{ $event->organizer }}" placeholder="" />
+                    @error('organizer')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                @enderror
-            </div> --}}
+                    @enderror
+                </div> --}}
 
 
-            <div class="mb-6">
-                <label for="ticket_price" class="inline-block text-lg mb-2">Ticket price</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="ticket_price"
-                    value="{{ $event->ticket_price }}" placeholder="€" />
-                @error('ticket_price')
+                <div class="mb-6">
+                    <label for="ticket_price" class="inline-block text-lg mb-2">Ticket price</label>
+                    <input type="text" class="border border-gray-200 rounded p-2 w-full" name="ticket_price"
+                        value="{{ $event->ticket_price }}" placeholder="€" />
+                    @error('ticket_price')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            <div class="mb-6">
-                <label for="tickets_available" class="inline-block text-lg mb-2">Tickets quantity</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tickets_available"
-                    value="{{ $event->tickets_available }}" placeholder="" />
-                @error('tickets_available')
+                <div class="mb-6">
+                    <label for="tickets_available" class="inline-block text-lg mb-2">Tickets quantity</label>
+                    <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tickets_available"
+                        value="{{ $event->tickets_available }}" placeholder="" />
+                    @error('tickets_available')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            <div class="mb-6">
-                <label for="event_image" class="inline-block text-lg mb-2">
-                    Image
-                </label>
-                <input type="file" class="border border-gray-200 rounded p-2 w-full" name="event_image" />
+                <div class="mb-6">
+                    <label for="event_image" class="inline-block text-lg mb-2">
+                        Image
+                    </label>
+                    <input type="file" class="border border-gray-200 rounded p-2 w-full" name="event_image" />
 
-            <img class="hidden w-80 mr-6 md:block"
-            src="{{ $event->event_image ? asset('storage/' . $event->event_image) : asset('/images/no_image.jpg') }}"
-            alt="" />
+                    <img class="hidden w-80 mr-6 md:block"
+                        src="{{ $event->event_image ? asset('storage/' . $event->event_image) : asset('/images/no_image.jpg') }}"
+                        alt="" />
 
-                @error('event_image')
+                    @error('event_image')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            <div class="mb-6">
-                <label for="description" class="inline-block text-lg mb-2">
-                    Description
-                </label>
-                <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10"
-                    placeholder="Event schedule...">{{ $event->description }}</textarea>
-                @error('description')
+                <div class="mb-6">
+                    <label for="description" class="inline-block text-lg mb-2">
+                        Description
+                    </label>
+                    <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10"
+                        placeholder="Event schedule...">{{ $event->description }}</textarea>
+                    @error('description')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            <div class="mb-6">
-                <label for="start_time" class="inline-block text-lg mb-2">Starting time</label>
-                <input type="datetime-local" class="border border-gray-200 rounded p-2 w-full" name="start_time"
-                    value="{{ $event->start_time }}" placeholder="" />
-                @error('start_time')
+                <div class="mb-6">
+                    <label for="start_time" class="inline-block text-lg mb-2">Starting time</label>
+                    <input type="datetime-local" class="border border-gray-200 rounded p-2 w-full" name="start_time"
+                        value="{{ $event->start_time }}" placeholder="" />
+                    @error('start_time')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            <div class="mb-6">
-                <label for="start_time" class="inline-block text-lg mb-2">Ending time</label>
-                <input type="datetime-local" class="border border-gray-200 rounded p-2 w-full" name="end_time"
-                    value="{{ $event->end_time }}" placeholder="" />
-                @error('end_time')
+                <div class="mb-6">
+                    <label for="start_time" class="inline-block text-lg mb-2">Ending time</label>
+                    <input type="datetime-local" class="border border-gray-200 rounded p-2 w-full" name="end_time"
+                        value="{{ $event->end_time }}" placeholder="" />
+                    @error('end_time')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            
 
-            <div class="mb-6 flex justify-center">
-                <a href="/events/{{ $event->id }}"> <i class="fa-solid fa-backward pr-4 text-3xl max-w-xs transition duration-300 ease-in-out hover:scale-125"></i></a>
-                <button class="bg-laravel text-black rounded py-2 px-4 hover:bg-black text-white ">
-                    Edit the event 
-                </button>
-            </div>
-        </form>
-    </div>
+
+                <div class="mb-6 flex justify-center">
+                    <a href="/events/{{ $event->id }}"> <i
+                            class="fa-solid fa-backward pr-4 text-3xl max-w-xs transition duration-300 ease-in-out hover:scale-125"></i></a>
+                    <button class="bg-laravel text-black rounded py-2 px-4 hover:bg-black text-white ">
+                        Edit the event
+                    </button>
+                </div>
+            </form>
+        </div>
         </div>
     </x-card>
- </body>
- @endsection
+</body>
+@endsection
