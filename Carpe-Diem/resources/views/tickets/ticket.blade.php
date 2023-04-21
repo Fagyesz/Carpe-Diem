16e68ed76b51c15bc1d548f36aedf40fbc0910bf
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-04-21 17:08:09 +0200

    Creating Ticket schema

---
+++ Carpe-Diem/resources/views/tickets/ticket.blade.php
@@ -0,0 +1,103 @@
+@extends('layout')
+
+@section('content')
+
+<div class="flex flex-col items-center justify-center min-h-screen bg-center bg-cover"
+	style="background-image: url(../images/single_listing_bg.jpg)">
+    <div class="pt-12 w-full pb-12">
+	<div class="max-w-md w-full h-full mx-auto z-10 bg-blue-800 rounded-3xl">
+		<div class="flex flex-col">
+			<div class="bg-white relative drop-shadow-2xl  rounded-3xl p-4 m-4">
+				<div class="flex-none sm:flex">
+					<div class="flex-auto justify-evenly">
+						<div class="flex items-center">
+							<div class="flex flex-col">
+								<div class="flex-auto text-xs text-gray-400 my-1">
+									<span class="mr-1 ">MO</span><span>19 22</span>
+								</div>
+								<div class="w-full flex-none text-lg text-blue-800 font-bold leading-none">COK</div>
+								<div class="text-xs">Cochi</div>
+
+							</div>
+							<div class="flex flex-col mx-auto">
+								<img src="../images/CD-logo2.png" class="w-24 p-1">
+
+								</div>
+								<div class="flex flex-col ">
+									<div class="flex-auto text-xs text-gray-400 my-1">
+										<span class="mr-1">MO</span><span>19 22</span>
+									</div>
+									<div class="w-full flex-none text-lg text-blue-800 font-bold leading-none">DXB</div>
+									<div class="text-xs">Dubai</div>
+
+								</div>
+							</div>
+							<div class="border-b border-dashed border-b-2 my-5 pt-5">
+								<div class="absolute rounded-full w-5 h-5 bg-blue-900 -mt-2 -left-2"></div>
+								<div class="absolute rounded-full w-5 h-5 bg-blue-900 -mt-2 -right-2"></div>
+							</div>
+							<div class="flex items-center mb-5 p-5 text-sm">
+								<div class="flex flex-col">
+									<span class="text-sm">Flight</span>
+									<div class="font-semibold">Airbus380</div>
+
+								</div>
+								<div class="flex flex-col ml-auto">
+									<span class="text-sm">Gate</span>
+									<div class="font-semibold">B3</div>
+
+								</div>
+							</div>
+							<div class="flex items-center mb-4 px-5">
+								<div class="flex flex-col text-sm">
+									<span class="">Board</span>
+									<div class="font-semibold">11:50AM</div>
+
+								</div>
+								<div class="flex flex-col mx-auto text-sm">
+									<span class="">Departs</span>
+									<div class="font-semibold">11:30Am</div>
+
+								</div>
+								<div class="flex flex-col text-sm">
+									<span class="">Arrived</span>
+									<div class="font-semibold">2:00PM</div>
+
+								</div>
+							</div>
+							<div class="border-b border-dashed border-b-2 my-5 pt-5">
+								<div class="absolute rounded-full w-5 h-5 bg-blue-900 -mt-2 -left-2"></div>
+								<div class="absolute rounded-full w-5 h-5 bg-blue-900 -mt-2 -right-2"></div>
+							</div>
+							<div class="flex items-center px-5 pt-3 text-sm">
+								<div class="flex flex-col">
+									<span class="">Passanger</span>
+									<div class="font-semibold">Ajimon</div>
+
+								</div>
+								<div class="flex flex-col mx-auto">
+									<span class="">Class</span>
+									<div class="font-semibold">Economic</div>
+
+								</div>
+								<div class="flex flex-col">
+									<span class="">Seat</span>
+									<div class="font-semibold">12 E</div>
+
+								</div>
+							</div>
+							<div class="flex flex-col py-5  justify-center text-sm ">
+								<h6 class="font-bold text-center">Boarding Pass</h6>
+
+								<div class="barcode h-14 w-0 inline-block mt-4 relative left-auto">{{-- qr code here --}}</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+	</div>
+</div>
+
+@endsection
