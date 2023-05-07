5b2d94bd549b1133c7da15c5103c686cad859d58
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-05-07 17:42:01 +0200

    Adding main site and post event to navbar

--- Carpe-Diem/resources/views/components/navbar.blade.php
+++ Carpe-Diem/resources/views/components/navbar.blade.php
@@ -4,9 +4,11 @@
             <div class="flex items-center">
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
+                        <a href="/" class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Main</a>
                         @foreach ($links as $link)
                             <a href="/{{ strtolower($link) }}" class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">{{ $link }}</a>
                         @endforeach
+                        <a href="/events/new_event" class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Post event</a>
                     </div>
                 </div>
             </div>
