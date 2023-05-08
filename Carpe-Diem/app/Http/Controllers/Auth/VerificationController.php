0a3c68e2a9c548a2301fb5b44cee69ab0c8e4f9b
Fagyesz <vinczef.o@gmail.com>
2023-05-08 07:00:42 +0100

    EmailVerifitaion controller

--- Carpe-Diem/app/Http/Controllers/Auth/VerificationController.php
+++ Carpe-Diem/app/Http/Controllers/Auth/VerificationController.php
@@ -3,26 +3,39 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Auth\Events\Verified;
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\VerifiesEmails;
 
 class VerificationController extends Controller
 {
-    public function verify(Request $request)
-    {
-        $user = User::find($request->route('id'));
-        if ($user->hasVerifiedEmail()) {
-            return redirect()->route('home');
-        }
-        if ($user->markEmailAsVerified()) {
-            event(new Verified($user));
-        }
-        return redirect()->route('home')->with('verified', true);
-    }
+    /*
+    |--------------------------------------------------------------------------
+    | Email Verification Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling email verification for any
+    | user that recently registered with the application. Emails may also
+    | be re-sent if the user didn't receive the original email message.
+    |
+     */
 
-    public function resend(Request $request)
+    use VerifiesEmails;
+
+    /**
+     * Where to redirect users after verification.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
-        $request->user()->sendEmailVerificationNotification();
-        return back()->with('resent', true);
+        $this->middleware('auth');
+        $this->middleware('signed')->only('verify');
+        $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 }
