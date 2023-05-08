d8ffe0cc50b13c6af251cd9d65d58b09dfbd5126
katonaBalintSandor <balintgamer14@gmail.com>
2023-05-08 20:57:02 +0200

    Changed search bar color

--- Carpe-Diem/resources/views/partials/search.blade.php
+++ Carpe-Diem/resources/views/partials/search.blade.php
@@ -1,5 +1,5 @@
 <form action="/events">
-    <div class="relative border-2 border-gray-100 m-4 rounded-lg bg-gray-300">
+    <div class="relative m-4 rounded-lg bg-yellow-300">
         <div class="absolute top-4 left-3">
             <i
                 class="fa fa-search text-gray-400 z-20 hover:text-gray-500"
@@ -8,7 +8,7 @@
         <input
             type="text"
             name="search"
-            class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none bg-gray-800 text-white"
+            class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none bg-yellow-200"
             placeholder="Search events..."
         />
         <div class="absolute top-2 right-2">
