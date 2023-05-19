17dbd9572c061811ed3ab1fd588b19ccfe8cf465
katonaBalintSandor <balintgamer14@gmail.com>
2023-05-19 13:59:28 +0200

    main page bug fixed

--- Carpe-Diem/resources/views/partials/footer.blade.php
+++ Carpe-Diem/resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <footer
 class="bottom-0 break-words left-0 w-full flex items-center justify-start font-bold bg-sky-400/95 text-white h-24 md:justify-center">
-<p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
+<p class="ml-2 pr-5">Copyright &copy; 2023, All Rights reserved</p>
 {{-- @if($admin) --}}
-<a href="/events/new_event" class="absolute right-10 block bg-gray-900 font-medium p-3 rounded-xl hover:shadow-lg transition duration-200 ease-in-out hover:bg-indigo-600 hover:text-white">Post a new event</a>
+<a href="/events/new_event" class="static right-10 block bg-gray-900 font-medium p-3 rounded-xl hover:shadow-lg transition duration-200 ease-in-out hover:bg-indigo-600 hover:text-white">Post a new event</a>
 {{-- @endif --}} 
 
 </footer>
