a5c6f984c61b74a0f43237e1e6327d4d5d31526e
Vincze Flórián <44016005+Fagyesz@users.noreply.github.com>
2023-05-26 13:11:20 +0000

    test

--- app/Http/Controllers/QRcodeControllerEndroid.php
+++ app/Http/Controllers/QRcodeControllerEndroid.php
@@ -16,10 +16,28 @@ use Illuminate\Support\Facades\DB;
 
 class QRcodeControllerEndroid extends Controller
 {
-    public function generate($url)
+    public function index()
     {
-        $qrCode = new QrCode($url);
-        header('Content-Type: '.$qrCode->getContentType());
-        return $qrCode->writeString();
+    	return view('qrcode.index');
+    }
+    
+    public function create()
+    {
+		$qrCode = new QrCode('techsolutionstuff.com');
+		$qrCode->setSize(300);
+		$qrCode->setMargin(10); 
+		$qrCode->setEncoding('UTF-8');
+		$qrCode->setWriterByName('png');
+		$qrCode->setErrorCorrectionLevel(ErrorCorrectionLevel::HIGH());
+		$qrCode->setForegroundColor(['r' => 0, 'g' => 0, 'b' => 0, 'a' => 0]);
+		$qrCode->setBackgroundColor(['r' => 255, 'g' => 255, 'b' => 255, 'a' => 0]);
+		$qrCode->setLogoSize(150, 200);
+		$qrCode->setValidateResult(false);		
+		$qrCode->setRoundBlockSize(true);
+		$qrCode->setWriterOptions(['exclude_xml_declaration' => true]);
+		header('Content-Type: '.$qrCode->getContentType());
+		$qrCode->writeFile(public_path('/qrcode.png'));
+
+		return redirect()->route('qrcode.index');
     }
 }
